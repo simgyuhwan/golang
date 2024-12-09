7d7a3371925672f0bb8c4e3950d61815c7728ee0
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-12-09 21:05:17 +0900

    task

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require golang.org/x/sync v0.10.0
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
--- mux.go
+++ mux.go
@@ -1,13 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"api/handler"
+	"api/store"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
 
-		_, _ = w.Write([]byte(`{"statue": "ok"}`))
-	})	
+	v := validator.New()
+	mux.Handle("/tasks", &handler.AddTask{Store: store.Tasks, Validator: v})
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
