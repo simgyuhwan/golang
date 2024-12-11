fbeab3e9cef341bf59458cd2074d2fdd5dc575d8
wizcrew-sgh <wizcrew-sgh@gmail.com>
2024-12-11 11:49:38 +0900

    login

---
+++ gormfiber/handler/auth_handler.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"gofiber/database"
+	"gofiber/model/entity"
+	"gofiber/model/request"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+func LoginHandler(c *fiber.Ctx) error {
+	loginRequest := request.LoginRequest{}
+	if err := c.BodyParser(&loginRequest); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message" : "fail",
+		})
+	}
+
+	validate := validator.New()
+	errValidate := validate.Struct(loginRequest)
+	if errValidate != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message" : "failed",
+			"error" : errValidate.Error(),
+		})
+	}
+
+	var user entity.User
+	err := database.DB.First(&user, "email = ?", loginRequest.Email).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message" : "wrong credential",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"token": "secret",
+	})
+}
--- gormfiber/route/index_route.go
+++ gormfiber/route/index_route.go
@@ -1,37 +1,20 @@
 package route
 
 import (
-	"fmt"
 	"gofiber/config"
 	"gofiber/handler"
+	"gofiber/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func middleware(ctx *fiber.Ctx) error {
-	token := ctx.Get("x-token")
-	if token != "secret"{
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message" : "unauthenticated",
-		})
-	}
-	return ctx.Next()
-}
-
 func RouteInit(r *fiber.App) {
 	r.Static("/public", config.ProjectRootPath + "/public/asset")
-	r.Get("/user", middleware,handler.UserHandlerGetAll)
+	r.Get("/user", middleware.Auth ,handler.UserHandlerGetAll)
 	r.Get("/user/:id", handler.UserHandlerGetById)
+	r.Post("/login", handler.LoginHandler)
 	r.Post("/user", handler.UserHandlerCreate)
 	r.Put("/user/:id", handler.UserHandlerUpdate)
 	r.Put("/user/:id/update-email", handler.UserHandlerUpdateEmail)
 	r.Delete("/user/:id", handler.UserHandlerDelete)
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
-	fmt.Printf("-----------------")
 }
