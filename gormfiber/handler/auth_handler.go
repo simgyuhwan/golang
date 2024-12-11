69288179663062c0ba1636ac1e23047c333440fe
wizcrew-sgh <wizcrew-sgh@gmail.com>
2024-12-11 16:37:52 +0900

    add jwt

--- gormfiber/handler/auth_handler.go
+++ gormfiber/handler/auth_handler.go
@@ -5,7 +5,10 @@ import (
 	"gofiber/model/entity"
 	"gofiber/model/request"
 	"gofiber/utils"
+	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
@@ -42,7 +45,21 @@ func LoginHandler(c *fiber.Ctx) error {
 		})
 	}
 
+	claims := jwt.MapClaims{}
+	claims["name"] = user.Name
+	claims["email"] = user.Email
+	claims["address"] = user.Address
+	claims["exp"] = time.Now().Add(time.Minute * 2).Unix()
+
+	token, errGenerateToken := utils.GenerateToken(&claims)
+	if errGenerateToken != nil {
+		log.Println(errGenerateToken)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "wrong credential",
+		})
+	}
+
 	return c.JSON(fiber.Map{
-		"token": "secret",
+		"token": token,
 	})
 }
